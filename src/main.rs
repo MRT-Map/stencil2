4c74a67e97ca57f4128b14eb85ec744204450dc1
7d <i.second.7d@gmail.com>
2022-07-11 18:29:35 +0800

    add ui

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-mod tile;
-mod utils;
+mod rendering;
+mod types;
+mod editor;
 
-use crate::tile::*;
-use crate::utils::get_cursor_world_pos;
+use rendering::tile::*;
+use rendering::utils::*;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 use bevy_mouse_tracking_plugin::{MainCamera, MousePosPlugin};
 use bevy_web_asset::WebAssetPlugin;
+use iyes_loopless::prelude::*;
+use crate::types::*;
 
 fn main() {
     #[cfg(target_arch = "wasm32")]
@@ -18,14 +22,53 @@ fn main() {
         })
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
-        .add_system(world_pos)
         .add_plugin(MousePosPlugin::SingleCamera)
-        .add_startup_system(setup)
+        .add_plugin(EguiPlugin)
         .insert_resource(Zoom(7.0))
+        .add_loopless_state(EditorState::Idle)
+        .add_startup_system(setup)
+        .add_system(ui)
+        .add_system(world_pos)
         .add_system(show_tiles)
         .add_system(mouse_drag)
         .add_system(mouse_zoom)
         .run();
+}
+
+fn ui(mut ctx: ResMut<EguiContext>) {
+    let mut namespace = "";
+    let mut id = "";
+    let mut display_name = "";
+    let mut description = "";
+    let mut tags = "";
+    let mut layer = 0.0;
+    egui::SidePanel::left("main")
+        .default_width(200.0)
+        .show(ctx.ctx_mut(), |ui| {
+            ui.heading(format!("Stencil v{}", env!("CARGO_PKG_VERSION")));
+            ui.end_row();
+            ui.add(egui::TextEdit::singleline(&mut namespace)
+                .hint_text("namespace"));
+            ui.add(egui::TextEdit::singleline(&mut id)
+                .hint_text("id"));
+            ui.end_row();
+            ui.add(egui::TextEdit::singleline(&mut display_name)
+                .hint_text("Displayed as"));
+            ui.end_row();
+            ui.add(egui::TextEdit::multiline(&mut description)
+                .hint_text("Description"));
+            ui.end_row();
+            ui.separator();
+            egui::ComboBox::from_label("Component type")
+                .show_ui(ui, |_|());
+            ui.end_row();
+            ui.add(egui::TextEdit::singleline(&mut tags)
+                .hint_text("Tags"));
+            ui.end_row();
+            ui.label("Layer:");
+            ui.add(egui::Slider::new(&mut layer, -10.0..=10.0));
+
+        });
 }
 
 fn world_pos(
--- src/rendering/tile.rs
+++ src/rendering/tile.rs
@@ -1,70 +1,12 @@
-use crate::utils::{
+use crate::rendering::utils::{
     get_cursor_world_pos, get_map_coords_of_edges, get_map_width_height, get_window_width_height,
 };
 use bevy::input::mouse::{MouseScrollUnit, MouseWheel};
 use bevy::prelude::*;
 use bevy::sprite::Anchor;
 use bevy_mouse_tracking_plugin::{MainCamera, MousePos};
-
-pub struct Zoom(pub f32);
-impl Zoom {
-    fn map_size(&self) -> f64 {
-        2f64.powf((8f32 - self.0) as f64) * 32f64
-    }
-    fn world_size(&self) -> i32 {
-        (2f64.powf((8f32 - self.0) as f64) * 32f64) as i32
-    }
-}
-
-#[derive(Component, Default, PartialEq, Copy, Clone, Debug)]
-pub struct TileCoord {
-    pub x: i32,
-    pub y: i32,
-    pub z: i8,
-}
-impl TileCoord {
-    fn from_world_coords(x: f64, y: f64, z: i8) -> Self {
-        Self {
-            x: (x / Zoom(z as f32).world_size() as f64) as i32,
-            y: (y / Zoom(z as f32).world_size() as f64) as i32,
-            z,
-        }
-    }
-
-    fn get_edges(&self) -> (f32, f32, f32, f32) {
-        (
-            self.x as f32 * Zoom(self.z as f32).world_size() as f32,
-            self.y as f32 * Zoom(self.z as f32).world_size() as f32,
-            (self.x + 1) as f32 * Zoom(self.z as f32).world_size() as f32,
-            (self.y + 1) as f32 * Zoom(self.z as f32).world_size() as f32,
-        )
-    }
-
-    fn url(&self) -> String {
-        let z = 2.0f64.powi((8 - self.z) as i32);
-        let x = self.x as f64;
-        let y = self.y as f64;
-
-        let group_x = ((x * z) as f64 / 32.0).floor() as i32;
-        let group_y = ((y * z) as f64 / 32.0).floor() as i32;
-
-        let num_in_group_x = x * z;
-        let num_in_group_y = y * z;
-
-        let mut zzz = "".to_string();
-        let mut i = 8;
-        while i > self.z {
-            zzz += "z";
-            i -= 1;
-        }
-
-        if !zzz.is_empty() {
-            zzz += "_"
-        };
-        format!("http://api.allorigins.win/raw?url=https%3A//dynmap.minecartrapidtransit.net/tiles/new/flat/{}_{}/{}{}_{}.png",
-            group_x, group_y, zzz, num_in_group_x, num_in_group_y)
-    }
-}
+use crate::types::TileCoord;
+use crate::Zoom;
 
 #[derive(Component)]
 pub struct Tile;

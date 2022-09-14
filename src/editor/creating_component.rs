2130c7f4a4ffa3e8868f2a19639070a4e465c6ae
7d <61975820+iiiii7d@users.noreply.github.com>
2022-09-14 23:59:17 +0800

    no longer places new comp if mouse is moving or on gui

--- src/editor/creating_component.rs
+++ src/editor/creating_component.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
-use bevy_mouse_tracking_plugin::MainCamera;
+use bevy_mouse_tracking_plugin::{MainCamera, MousePos};
+use bevy_prototype_lyon::entity::ShapeBundle;
 use iyes_loopless::prelude::*;
 
 use crate::{
@@ -14,7 +15,8 @@ use crate::{
         DeselectQuery, EditorState, pla::ComponentCoords, SelectQuery, skin::Skin,
     },
 };
-use crate::editor::bundles::component::{ComponentBundle, CreatedComponent, EditorComponent, SelectedComponent};
+use crate::editor::bundles::component::{ComponentBundle, CreatedComponent, EditorComponent};
+use crate::types::Label;
 
 #[allow(clippy::too_many_arguments, clippy::type_complexity)]
 pub fn create_component(
@@ -30,30 +32,39 @@ pub fn create_component(
     windows: Res<Windows>,
     mut camera: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
     hovering_over_gui: Res<HoveringOverGui>,
+    mut previous_mouse_pos: Local<Option<MousePos>>,
+    mouse_pos: Res<MousePos>
 ) {
     // TODO check if screen is moved
     if let EditorState::CreatingComponent(type_) = &state.0 {
         let (camera, transform): (&Camera, &GlobalTransform) = camera.single_mut();
-        let mouse_pos = if let Some(mp) = get_cursor_world_pos(&windows, camera, transform) {
+        let mouse_world_pos = if let Some(mp) = get_cursor_world_pos(&windows, camera, transform) {
             mp
         } else {
             return;
         };
+        if buttons.just_pressed(MouseButton::Left) || buttons.just_pressed(MouseButton::Right) {
+            *previous_mouse_pos = Some(*mouse_pos)
+        }
         if buttons.just_released(MouseButton::Left) && !hovering_over_gui.0 {
+            if let Some(previous_mouse_pos) = *previous_mouse_pos {
+                if previous_mouse_pos != *mouse_pos { return }
+            }
+            *previous_mouse_pos = None;
             if *type_ == ComponentType::Point {
                 let mut new_point = ComponentBundle::new(
                     EditorComponent::new(type_.to_owned()),
-                    mouse_pos.as_ivec2(),
+                    mouse_world_pos.as_ivec2(),
                 );
                 new_point.update_shape(&skin);
                 deselect(&mut commands, &set.p1());
-                commands.spawn_bundle(new_point).insert(SelectedComponent);
+                commands.spawn_bundle(new_point);
                 return;
             }
             if set.p0().is_empty() {
                 let mut new_comp = ComponentBundle::new(
                     EditorComponent::new(type_.to_owned()),
-                    mouse_pos.as_ivec2(),
+                    mouse_world_pos.as_ivec2(),
                 );
                 new_comp.update_shape(&skin);
                 commands.spawn_bundle(new_comp).insert(CreatedComponent);
@@ -63,7 +74,7 @@ pub fn create_component(
                     created_query.single_mut();
                 match data.get_type(&skin).unwrap() {
                     ComponentType::Line | ComponentType::Area => {
-                        coords.0.push(mouse_pos.as_ivec2());
+                        coords.0.push(mouse_world_pos.as_ivec2());
                         commands.entity(entity).insert_bundle(data.get_shape(
                             (*coords).to_owned(),
                             &skin,
@@ -73,15 +84,18 @@ pub fn create_component(
                     ComponentType::Point => unreachable!(),
                 }
             }
-        } else if buttons.just_released(MouseButton::Right) {
-            // or double left-click?
+        } else if buttons.just_released(MouseButton::Right) && !hovering_over_gui.0 {
+            if let Some(previous_mouse_pos) = *previous_mouse_pos {
+                if previous_mouse_pos != *mouse_pos { return }
+            }
+            *previous_mouse_pos = None;
             select_query(&mut commands, &mut set.p2());
         } else if *type_ != ComponentType::Point && !set.p0().is_empty() {
             let mut created_query = set.p0();
             let (data, coords, entity): (&EditorComponent, Mut<ComponentCoords>, Entity) =
                 created_query.single_mut();
             let mut coords = coords.to_owned();
-            coords.0.push(mouse_pos.as_ivec2());
+            coords.0.push(mouse_world_pos.as_ivec2());
             commands
                 .entity(entity)
                 .insert_bundle(data.get_shape(coords, &skin, false));
@@ -90,18 +104,15 @@ pub fn create_component(
 }
 
 pub fn clear_created_component(
-    mut commands: Commands,
-    created_query: CreatedQuery,
-    skin: Res<Skin>,
-    state: Res<CurrentState<EditorState>>,
+    commands: &mut Commands,
+    created_query: &CreatedQuery,
+    skin: &Res<Skin>,
 ) {
-    if matches!(&state.0, EditorState::CreatingComponent(_)) {
-        return;
-    }
     for (data, coords, entity) in created_query.iter() {
         commands
             .entity(entity)
-            .insert_bundle(data.get_shape(coords.to_owned(), &skin, false))
+            .remove_bundle::<ShapeBundle>()
+            .insert_bundle(data.get_shape(coords.to_owned(), skin, false))
             .remove::<CreatedComponent>();
     }
 }
@@ -112,8 +123,10 @@ impl Plugin for CreateComponentPlugin {
         app.add_system_set(
             ConditionSet::new()
                 .run_not_in_state(EditorState::Loading)
+                .after(Label::ToolbarUi)
+                .after(Label::MenuUi)
+                .after(Label::ComponentPanelUi)
                 .with_system(create_component)
-                .with_system(clear_created_component)
                 .into(),
         );
     }
--- src/editor/ui/toolbar.rs
+++ src/editor/ui/toolbar.rs
@@ -6,70 +6,41 @@ use crate::{
     editor::ui::HoveringOverGui,
     types::{ComponentType, EditorState},
 };
+use crate::editor::creating_component::clear_created_component;
+use crate::types::CreatedQuery;
+use crate::types::skin::Skin;
 
 pub fn ui(
     mut ctx: ResMut<EguiContext>,
-    mut commands: Commands,
+    mut _commands: Commands,
     mut hovering_over_gui: ResMut<HoveringOverGui>,
     mut cv: Local<&'static str>,
+    _created_query: CreatedQuery,
+    _skin: Res<Skin>,
 ) {
     let mut current_value = *cv;
     let panel = egui::TopBottomPanel::top("toolbar").show(ctx.ctx_mut(), |ui| {
         egui::menu::bar(ui, |ui| {
-            if ui
-                .selectable_value(&mut current_value, "", "Select")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::Idle))
+            macro_rules! button {
+                ($value:literal, $text:literal, $next_state:expr) => {
+                    if ui
+                        .selectable_value(&mut current_value, $value, $text)
+                        .clicked()
+                    {
+                        clear_created_component(&mut _commands, &_created_query, &_skin);
+                        _commands.insert_resource(NextState($next_state));
+                    }
+                }
             }
-            if ui
-                .selectable_value(&mut current_value, "point", "Point")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::CreatingComponent(
-                    ComponentType::Point,
-                )))
-            }
-            if ui
-                .selectable_value(&mut current_value, "line", "Line")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::CreatingComponent(
-                    ComponentType::Line,
-                )))
-            }
-            if ui
-                .selectable_value(&mut current_value, "area", "Area")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::CreatingComponent(
-                    ComponentType::Area,
-                )))
-            }
-            if ui
-                .selectable_value(&mut current_value, "edit_nodes", "Edit Nodes")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::EditingNodes))
-            };
-            if ui
-                .selectable_value(&mut current_value, "move", "Move Components")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::MovingComponent))
-            };
-            if ui
-                .selectable_value(&mut current_value, "rotate", "Rotate Components")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::RotatingComponent))
-            };
-            if ui
-                .selectable_value(&mut current_value, "delete", "Delete Components")
-                .clicked()
-            {
-                commands.insert_resource(NextState(EditorState::DeletingComponent))
-            };
+
+            button!("", "Select", EditorState::Idle);
+            button!("point", "Point", EditorState::CreatingComponent(ComponentType::Point));
+            button!("line", "Line", EditorState::CreatingComponent(ComponentType::Line));
+            button!("area", "Area", EditorState::CreatingComponent(ComponentType::Area));
+            button!("edit_nodes", "Edit Nodes", EditorState::EditingNodes);
+            button!("move", "Move Components", EditorState::MovingComponent);
+            button!("rotate", "Rotate Components", EditorState::RotatingComponent);
+            button!("delete", "Delete Components", EditorState::DeletingComponent);
         });
     });
     if panel.response.hovered() {

0997bd924eced5ce3f18499dccdc1b94a4bfe18c
7d <61975820+iiiii7d@users.noreply.github.com>
2022-10-24 13:29:40 +0800

    show popup when deleting a com of more than 5 nodes

--- src/component_tools/deleting.rs
+++ src/component_tools/deleting.rs
@@ -1,12 +1,30 @@
 use bevy::prelude::*;
 use iyes_loopless::prelude::*;
+use native_dialog::{MessageDialog, MessageType};
 
-use crate::{cursor::mouse_events::MouseEvent, setup::EditorState};
+use crate::{
+    cursor::mouse_events::MouseEvent,
+    pla2::component::{EditorCoords, PlaComponent},
+    setup::EditorState,
+};
 
 #[tracing::instrument(skip_all)]
-pub fn delete_component_sy(mut events: EventReader<MouseEvent>, mut commands: Commands) {
+pub fn delete_component_sy(
+    mut events: EventReader<MouseEvent>,
+    mut commands: Commands,
+    query: Query<(&PlaComponent<EditorCoords>, Entity)>,
+    _non_send: Option<NonSend<()>>,
+) {
     for event in events.iter() {
         if let MouseEvent::LeftClick(Some(e), _) = event {
+            let pla: &PlaComponent<EditorCoords> = query.iter().find(|(_, a)| a == e).unwrap().0;
+            if pla.nodes.len() > 5 && !MessageDialog::default()
+                    .set_title("This component has more than 5 nodes, are you sure you want to delete?")
+                    .set_text("We have not implemented undoing and redoing yet so your progress may be lost!")
+                    .set_type(MessageType::Warning)
+                    .show_confirm().unwrap() {
+                continue
+            }
             info!(?e, "Deleting entity");
             commands.entity(*e).despawn_recursive();
         }
@@ -17,11 +35,6 @@ pub struct DeleteComponentPlugin;
 
 impl Plugin for DeleteComponentPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set(
-            ConditionSet::new()
-                .run_in_state(EditorState::DeletingComponent)
-                .with_system(delete_component_sy)
-                .into(),
-        );
+        app.add_system(delete_component_sy.run_in_state(EditorState::DeletingComponent));
     }
 }

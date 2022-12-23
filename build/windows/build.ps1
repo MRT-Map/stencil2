ec1914f27c10dd84d8988a6f59ac350d48dab4c6
7d <61975820+iiiii7d@users.noreply.github.com>
2022-12-24 01:01:13 +0800

    modify build.ps1 to use relative paths

--- build/windows/build.ps1
+++ build/windows/build.ps1
@@ -2,5 +2,5 @@ $dir = $PSScriptRoot
 Set-Location "$dir\..\.."
 #Invoke-WebRequest -Uri https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe -OutFile wix311.exe
 cargo install cargo-wix
-cargo wix init -l "$dir\..\..\LICENSE" -e "$dir\..\..\LICENSE" -o "$dir" --force --product-icon "$dir\..\macos\AppIcon.iconset\icon_512x512@2x.png" -v
-cargo wix -I "$dir\main.wxs" -v
+cargo wix init -o "$dir" --force --product-icon ".\build\macos\AppIcon.iconset\icon_512x512@2x.png" -v -e ".\LICENSE" -l ".\LICENSE"
+cargo wix -I ".\build\windows\main.wxs" -v

27e21c5f9e0a74e01bd8596ebb0ec355ffdd0c27
7d <61975820+iiiii7d@users.noreply.github.com>
2022-12-24 02:02:25 +0800

    complete windows msi installation, i hope

--- build/windows/build.ps1
+++ build/windows/build.ps1
@@ -1,6 +1,5 @@
 $dir = $PSScriptRoot
 Set-Location "$dir\..\.."
-#Invoke-WebRequest -Uri https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe -OutFile wix311.exe
+Invoke-WebRequest -Uri https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe -OutFile wix311.exe
 cargo install cargo-wix
-cargo wix init -o "$dir" --force --product-icon ".\build\macos\AppIcon.iconset\icon_512x512@2x.png" -v -e ".\LICENSE" -l ".\LICENSE"
 cargo wix -I ".\build\windows\main.wxs" -v

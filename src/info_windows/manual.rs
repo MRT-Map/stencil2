f757330aa6e60e9360b16d0fb5da9d4293f1fc3a
7d <61975820+iiiii7d@users.noreply.github.com>
2023-05-31 14:42:38 +0800

    redirect to online manual

--- src/info_windows/manual.rs
+++ src/info_windows/manual.rs
@@ -15,15 +15,11 @@ pub fn manual_asy(mut actions: EventReader<Action>, mut popup: EventWriter<Arc<P
                     egui::Window::new("Manual")
                         .collapsible(false)
                         .anchor(egui::Align2::CENTER_CENTER, egui::Vec2::ZERO)
-                        .vscroll(true)
+                        .resizable(false)
                 },
                 |_, ui, _, show| {
-                    let mut cache = CommonMarkCache::default();
-                    CommonMarkViewer::new("viewer").show(
-                        ui,
-                        &mut cache,
-                        include_str!("../../manual.md"),
-                    );
+                    ui.label("Our online manual is available here:");
+                    ui.hyperlink("https://github.com/MRT-Map/stencil2/wiki");
                     if ui.button("Close").clicked() {
                         *show = false;
                     }

f46b549bcb26e2ddced3c8aace6e1a130b2b4a90
7d <61975820+iiiii7d@users.noreply.github.com>
2024-08-15 00:38:15 +0800

    Update brew.rb

--- build/macos/brew.rb
+++ build/macos/brew.rb
@@ -1,5 +1,5 @@
 cask "stencil2" do
-  version "$VERSION"
+  version "2.2.4"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   url "https://github.com/MRT-Map/stencil2/releases/download/v#{version}/stencil2.dmg"

36b9728ee95388ab4b815da560dffbdec3bfd3a1
7d <61975820+iiiii7d@users.noreply.github.com>
2024-08-14 23:34:09 +0800

    Update build.ps1

--- build/windows/build.ps1
+++ build/windows/build.ps1
@@ -2,6 +2,8 @@ $dir = $PSScriptRoot
 $ErrorActionPreference = "Stop"
 Set-Location "$dir\..\.."
 
+git config --system core.longpaths true
+
 cargo +nightly build --release
 
 #Invoke-WebRequest -Uri https://github.com/wixtoolset/wix3/releases/download/wix3112rtm/wix311.exe -OutFile wix311.exe
